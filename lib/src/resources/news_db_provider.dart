aaccb4c7866aa9969f34baf7b90f37598a9388c2
Ian <ian@Deans-MacBook-Pro.local>
2019-03-22 05:16:49 -0500

    bug no idea fix

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -56,7 +56,11 @@ class NewsDbProvider implements Source, Cache {
 
   @override
   Future<int> addItem(ItemModel item) {
-    return db.insert("Items", item.toMapForDb());
+    return db.insert(
+      "Items",
+      item.toMapForDb(),
+      conflictAlgorithm: ConflictAlgorithm.ignore,
+    );
   }
 }
 
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/stories_provider.dart';
+import '../widgets/news_list_tile.dart';
+
 class NewsList extends StatelessWidget {
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return NewsListTile(
+              itemId: snapshot.data[index],
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Center(
-        child: Text('News App'),
-      ),
+      body: buildList(bloc),
     );
   }
 }
